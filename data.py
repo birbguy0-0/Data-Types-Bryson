18b6a325ffabcb8ae86a2159a932e7544f9a6e61
birbguy0-0 <brysonz2@nycstudents.net>
2026-09-18 09:20:49 -0400

    data

--- data.py
+++ data.py
@@ -1,3 +1,36 @@
-x = 3
-y = float(3)
-print(x,y)
+# # x = 3.14159265
+# # y = float(3)
+# # print(x,y)
+# values = [1,2.23,5,7,2,30,15]
+# # print(values)
+# # for i in values:
+# #     print(i)
+# print(values[0])
+# print(values[6])
+
+# x = "this is a thing"
+# y= x.split( )
+# z = y[0]
+# print(y)
+# print(z)
+
+
+#Question: Using the "input" method in Python, ask a user to input a sentence. Then develop a function that accepts a the user input and will tell you how many words are in that string. First write out your plan in Pseudo-code using comments. Then craft the function.
+
+# count = 0
+# #Asks the question
+# sentence = input("Type a Sentence: ")
+# print(sentence)
+# #Breaks it up
+# word = sentence.split()
+# print(word)
+# #Counts the words
+# for i in word:
+#     count += 1
+# print(count)
+
+#Mad Lib Project
+
+noun = input("Type a noun: ")
+verb = input("Type a verb: ")
+adjective = input)("Type an adjective")
---
+++ important.py
@@ -0,0 +1,20 @@
+#String for characters
+name = "Bryson"
+#Input asks the user a question and records the answer
+#What we write in input is what user sees
+
+bill = input("How much was the bill")
+print(bill)
+
+if bill == "10":
+    print("Match")
+else:
+    print("No Match")
+# integer for whole numbers
+amt = 100
+#Float uses decimals
+#amt_two = 99.99
+
+#Boolean
+x = True
+y = False
